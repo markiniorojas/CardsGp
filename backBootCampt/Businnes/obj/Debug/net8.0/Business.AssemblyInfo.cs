06cf3dbf6504f3036a3a6665c370911062d01d15
markiniorojas <markiniorojas@gmail.com>
2025-08-02 15:52:05 -0500

    MER mejorado

--- backBootCampt/Businnes/obj/Debug/net8.0/Business.AssemblyInfo.cs
+++ backBootCampt/Businnes/obj/Debug/net8.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72935ba34330e99cadd8e7b060c81f237102f85c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
