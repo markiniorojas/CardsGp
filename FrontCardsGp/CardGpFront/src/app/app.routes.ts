ed330f3a87b6392c0efda4a4e6ef75d838da3b48
esteban11PM <epalomar280@gmail.com>
2025-08-04 01:04:33 -0500

    Maquetación de la interfaz de loobby

--- FrontCardsGp/CardGpFront/src/app/app.routes.ts
+++ FrontCardsGp/CardGpFront/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'lobby', pathMatch: 'full' },
+  { path: 'lobby', loadComponent: () => import('./features/lobby/components/lobby/lobby.component').then(m => m.LobbyComponent) },
+//   { path: 'game', loadChildren: () => import('./features/game/game.module').then(m => m.GameModule) },
+  { path: '**', redirectTo: 'lobby' }
+];
---
+++ FrontCardsGp/CardGpFront/src/app/features/lobby/components/add-player-button/add-player-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-player-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-player-button.component.html',
+  styleUrl: './add-player-button.component.css'
+})
+export class AddPlayerButtonComponent {
+
+}
